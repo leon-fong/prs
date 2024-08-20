e22c298215ca37ba4cd95e6fea0eff62ee8520a8
flm <ooohmydawn@hotmail.com>
2024-08-20 10:07:36 +0800

    feat: add footer

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,24 @@ useDark()
 </script>
 
 <template>
-  <NuxtPage />
+  <div class="min-h-[100svh] flex flex-col justify-stretch">
+    <NuxtPage />
+    <footer class="mt-auto p-2 text-center text-sm opacity-75 hover:opacity-100">
+      <a
+        class="hover:underline"
+        href="https://github.com/leon-fong/pr.leonfong.me"
+      >
+        source
+      </a>
+      &middot;
+      inspiration <a
+        class="font-semibold hover:underline"
+        href="https://x.com/antfu7"
+      >
+        @antfu7
+      </a>
+    </footer>
+  </div>
 </template>
 
 <style>
