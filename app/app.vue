219a53f298d12f93c408f4316865c37364ef0dc9
flm <ooohmydawn@hotmail.com>
2024-08-21 21:54:40 +0800

    fix: dark mode

--- app/app.vue
+++ app/app.vue
@@ -4,7 +4,7 @@ useDark()
 
 <template>
   <div fixed left-0 top-0 h-full w-full class="-z-10">
-    <div class="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] bg-white" />
+    <div class="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] bg-white dark:bg-dark" />
   </div>
 
   <div class="min-h-[100svh] flex flex-col justify-stretch">
